11aa499792136a524da8a0b639973fb824b3a02b
Tuhin Sarkar <tuhinsarkar.me@gmail.com>
2026-05-02 17:59:01 +0000

    Adding Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -122,7 +122,30 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className="overflow-x-hidden">
       <head>
-        {/* font awesome css was removed entirely */}
+        {/* Google tag */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4RSPP02CRJ"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4RSPP02CRJ');
+            `,
+          }}
+        />
+        {/* Microsoft Clarity */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "wkxbv9d87m");
+            `,
+          }}
+        />
       </head>
       <body
         suppressHydrationWarning
